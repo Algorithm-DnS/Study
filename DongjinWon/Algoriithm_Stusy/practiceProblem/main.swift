2a81ca798e84911966a6fb9d7667e1b8286ced15
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-01-10 15:58:59 +0900

    :그리디 기출문제 4,5

--- DongjinWon/Algoriithm_Stusy/practiceProblem/main.swift
+++ DongjinWon/Algoriithm_Stusy/practiceProblem/main.swift
@@ -21,16 +21,79 @@ import Foundation
 //}
 //print("\(result)")
 //MARK: - 그리디 A02
-var input = Array(readLine()!).map{Int(String($0))!}
-input.sort()
-var result = 0
+//var input = Array(readLine()!).map{Int(String($0))!}
+//input.sort()
+//var result = 0
+//
+//for i in input{
+//    if i == 1 || result <= 1 {
+//        result+=i
+//    }else {
+//        result*=i
+//    }
+//}
+//print("\(result)")
+//
+//MARK: - 그리디 A03
+//var input = Array(readLine()!)
+//var count0 = 0
+//var count1 = 0
+//var sum = 0
+//
+//if input[0] == "0" {
+//    count0+=1
+//}else {
+//    count1+=1
+//}
+//for i in 0..<input.count-1 {
+//    if input[i] != input[i+1]{
+//        if input[i+1] == "0"{
+//            count0+=1
+//        }else {
+//            count1+=1
+//        }
+//    }
+//}
+//print("\(min(count0, count1))")
 
-for i in input{
-    if result == 0 {
-        result+=i
-    }else {
-        result*=i
-    }
-}
-print("\(result)")
-
+//MARK: - 그리드 A04
+//let N = Int(readLine()!)!
+//var input = readLine()!.split(separator: " ").map{Int($0)!}
+//var target = 1
+//input.sort()
+//for i in input {
+//    if target < i {
+//        break
+//    }
+//    target+=i
+//}
+//print("\(target)")
+//MARK: - 그리드 A05 - 내풀이
+//let input = readLine()!.split(separator:" ").map{Int($0)!}
+//let N = input[0]
+//let M = input[1]
+//var result = 0
+//let K  = readLine()!.split(separator:" ").map{Int($0)!}
+//for i in 0..<N{
+//    for j in i+1..<N{
+//        if K[i] != K[j] {
+//            result+=1
+//        }
+//    }
+//}
+//print("\(result)")
+//MARK: - 해설풀이
+//let input = readLine()!.split(separator:" ").map{Int($0)!}
+//let K  = readLine()!.split(separator:" ").map{Int($0)!}
+//var N = input[0]
+//let M = input[1]
+//var arr = [Int](repeating: 0, count: 10)
+//var result = 0
+//for i in K {
+//    arr[i]+=1
+//}
+//for i in 1...M{
+//    N -= arr[i]
+//    result+=arr[i]*N
+//}
+//print("\(result)")
