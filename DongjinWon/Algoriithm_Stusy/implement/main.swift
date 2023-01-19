804ceebb0fa22244bfcfca5a9bd5b6d7475df430
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-01-19 20:56:25 +0900

    기출 Q07 럭키스트레이트

--- DongjinWon/Algoriithm_Stusy/implement/main.swift
+++ DongjinWon/Algoriithm_Stusy/implement/main.swift
@@ -70,3 +70,20 @@ import Foundation
 //    }
 //}
 //print("\(count)")
+//MARK: - Q07 럭키스트레이트
+let input = Array(readLine()!).map{Int(String($0))!}
+let slicePoint = input.count/2
+let firstSlice = input.prefix(slicePoint)
+var firstSum = 0
+let secondSlice = input.suffix(slicePoint)
+var secondSum = 0
+for i in 0..<slicePoint{
+    firstSum += firstSlice[i]
+    secondSum += secondSlice[i+slicePoint]
+}
+if firstSum == secondSum {
+    print("LUCKY")
+}else{
+    print("READY")
+}
+
