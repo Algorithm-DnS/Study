74cecf928367cc9577e6f087d92733cf8eb52810
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-02-08 03:29:38 +0900

    백준 18352 특정 거리의 도시찾기 문제 , 시간초과해결하지못함..

--- DongjinWon/Algoriithm_Stusy/DFS/BFS/main.swift
+++ DongjinWon/Algoriithm_Stusy/DFS/BFS/main.swift
@@ -101,44 +101,82 @@ import Foundation
 //print("\(answer)")
 
 //MARK: - 미로탈출 BFS 백준 2178
-let input = readLine()!.split(separator: " ").map{Int($0)!}
-let (N , M)  = (input[0] , input[1])
-var graph = [[Int]]()
-for _ in 0..<N{
-    graph.append(readLine()!.map{Int(String($0))!})
-}
-let dx = [-1,1,0,0] // 아래에 이렇게 설정한 이유 - 이동할방향
-let dy = [0,0,-1,1]
-var queue = [[Int]]()
-bfs(0, 0) // bfs가장 가까운 노드를 탐색할수 있기때문 0,0이 기준
-// 목표가 항상 오른쪽 끝임으로 다른 노드의 count가 증가해도 상관이 없음.
-var nx = 0 // 현재 노드의 x좌표
-var ny = 0 // 현재 노드의 y좌표
-func bfs(_ x : Int,_ y : Int){
-    queue.append([x,y])
-    while queue.count != 0 {
-        let preNode : [Int] = queue.removeFirst() // dequeque개념
-                //preNode : 4방향으로 이동하기 전의 좌표
-        let x = preNode[0]
-        let y = preNode[1]
-        for i in 0..<4 {
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 || ny < 0 || nx >= N || ny >= M{
-                continue // 맵을 넘어설때
-            }
-            else if graph[nx][ny] == 0 {
-                continue //통과하지 못하는경우
-            }
-            else if graph[nx][ny] == 1{ //방문가능 노드
-                graph[nx][ny] = graph[x][y]+1 //현재 노드의 좌표에서 축적된거리 = 이전 노드좌표에 축적된 거리 +1
-                                //-> 지나온 거리만큼 노드에 축적됨
-                queue.append([nx,ny])
-                            //-> BFS 인접노드 방문
-            }
-        }
-    }
-    print("\(graph[N-1][M-1])")
-
-}
-
+//let input = readLine()!.split(separator: " ").map{Int($0)!}
+//let (N , M)  = (input[0] , input[1])
+//var graph = [[Int]]()
+//for _ in 0..<N{
+//    graph.append(readLine()!.map{Int(String($0))!})
+//}
+//let dx = [-1,1,0,0] // 아래에 이렇게 설정한 이유 - 이동할방향
+//let dy = [0,0,-1,1]
+//var queue = [[Int]]()
+//bfs(0, 0) // bfs가장 가까운 노드를 탐색할수 있기때문 0,0이 기준
+//// 목표가 항상 오른쪽 끝임으로 다른 노드의 count가 증가해도 상관이 없음.
+//var nx = 0 // 현재 노드의 x좌표
+//var ny = 0 // 현재 노드의 y좌표
+//func bfs(_ x : Int,_ y : Int){
+//    queue.append([x,y])
+//    while queue.count != 0 {
+//        let preNode : [Int] = queue.removeFirst() // dequeque개념
+//                //preNode : 4방향으로 이동하기 전의 좌표
+//        let x = preNode[0]
+//        let y = preNode[1]
+//        for i in 0..<4 {
+//            nx = x + dx[i]
+//            ny = y + dy[i]
+//            if nx < 0 || ny < 0 || nx >= N || ny >= M{
+//                continue // 맵을 넘어설때
+//            }
+//            else if graph[nx][ny] == 0 {
+//                continue //통과하지 못하는경우
+//            }
+//            else if graph[nx][ny] == 1{ //방문가능 노드
+//                graph[nx][ny] = graph[x][y]+1 //현재 노드의 좌표에서 축적된거리 = 이전 노드좌표에 축적된 거리 +1
+//                                //-> 지나온 거리만큼 노드에 축적됨
+//                queue.append([nx,ny])
+//                            //-> BFS 인접노드 방문
+//            }
+//        }
+//    }
+//    print("\(graph[N-1][M-1])")
+//
+//}
+//
+//MARK: - Q15 특정 거리의 도시 찾기 백준 18352 -> 시간 초과 해결하지못함 ,, 다른 사람 코드봐도 어떻게 시간초과를 해결하는지 모르겠음 ..
+//let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+//let (N,M,K,X) = (input[0] , input[1] , input[2] , input[3])
+//var graph : [[Int]] = Array(repeating: [], count: N+1)
+//var distance : [Int] = Array(repeating: 0, count: N+1)
+//var queue : [Int] = []
+//for _ in 0..<M{
+//    let input2 = readLine()!.split(separator: " ").map{Int(String($0))!}
+//    let(a,b) = (input2[0],input2[1])
+//    graph[a].append(b)
+//}
+//func bfs(_ start : Int, targetGraph : [[Int]]){
+//    queue.append(start)
+//    while queue.count != 0 {
+//        let now = queue.removeFirst() // 현재 node
+//        for next in targetGraph[now]{ // next 현재 node에서 갈수있는 node
+//            if distance[next] == 0 { // 갈수 있는 노드
+//                distance[next] = distance[now]+1 // 현재 노드 + 1 = 다음 노드
+//                queue.append(next)
+//            }
+//        }
+//    }
+//    
+//    
+//}
+//bfs(X, targetGraph: graph)
+//var check = false
+//for i in 0..<distance.count{
+//    if distance[i] == K {
+//        print("\(i)")
+//        check = true
+//    }
+//}
+//if check == false{
+//    print("-1")
+//}
+//
+//
